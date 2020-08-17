d349e4c73f252ec7dd56025346079a725b297776
Dival Banerjee <bandiv2011@gmail.com>
2020-08-17 03:39:42 -0400

    Finished main page design
    finished main page design

--- vuecason/src/gatsby-plugin-theme-ui/index.js
+++ vuecason/src/gatsby-plugin-theme-ui/index.js
@@ -8,6 +8,7 @@ export default {
     ...dark.colors,
     background: '#000',
     primary: '#FF928B',
+    accent: '#F2A365',
   },
 
 }
--- vuecason/src/pages/index.js
+++ vuecason/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withPrefix } from 'gatsby';
 import { ThemeProvider } from 'emotion-theming'
+import { Divider } from 'theme-ui'
 
 import { 
   Flex,
@@ -10,6 +11,7 @@ import {
   Heading,
   Button,
   Image,
+  Text,
 } from 'rebass'
 
 export default function Home() {
@@ -39,9 +41,60 @@ export default function Home() {
                  paddingLeft: '12px',
                  paddingBottom: '150px',
               }}>🦆 vuecason</Heading>
+
+            <div  
+              style={{
+                paddingLeft:'104px',
+              }}>
+              <Text
+                fontSize={[5]}
+                mb={5}
+                fontWeight='bold'>
+                  We're developing rapid metal 3D printers. 
+              </Text>
+              <Text
+                fontSize={[5]}
+                mb={4}>
+                  Minimal post processing required. 
+              </Text>
+              <Text
+                color='accent'
+                fontSize={[4]}
+                ml={5}
+                mb={4}>
+                  No debinding.
+              </Text>
+              <Text
+                color='accent'
+                fontSize={[4]}
+                ml={5}
+                mb={4}>
+                  No sintering.
+              </Text> 
+              <Text
+                color='accent'
+                fontSize={[4]}
+                ml={5}
+                mb={3}>
+                  What previously took 90 hours will take 10.
+              </Text>  
+              <Divider />
+              <Button variant="outline" //FIXME: Why tf does this not work
+                mt={3}
+                sx={{
+                  fontWeight:'bold',
+                }}
+              >Manifesto</Button>
               
-            <Button variant="outline"
-            >read the manifesto</Button>
+              <Button variant="outline" //FIXME: Why tf does this not work
+                mt={3}
+                ml={3}
+                href='https://form.typeform.com/to/gPL8zy'
+                sx={{
+                  fontWeight:'bold',
+                }}
+              >Stay in touch</Button>
+            </div>
           </div>
     </Flex>    
   );
