136f93b248775a99d9ad013e51be209ae35eb4b4
Dival Banerjee <bandiv2011@gmail.com>
2020-10-05 05:06:10 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-97cf2067a9961879de5e.js"
+    "url": "webpack-runtime-5d46131a3f0b2c1ba9bc.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2f7348c10b7996f156e48495485a7d69"
+    "revision": "82e54b32e3af22f6517a7e780fe90968"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
